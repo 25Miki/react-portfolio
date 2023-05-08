5efae1021a8b7ffee6cfc5ca3b65e2a990b0d9ea
25Miki <mariamarchantellopis@gmail.com>
2023-05-08 23:11:36 +0200

    lunes

--- src/sections/header/Header.jsx
+++ src/sections/header/Header.jsx
@@ -1,8 +1,27 @@
+import HeaderImage from '../../assets/header.jpg'
 import './header.css'
+import data from './data'
 
 const Header = () => {
     return (
-        <header id='header' ><h2>Header</h2></header>
+        <header id="header" >
+            <div className="container header__container">
+                <div className="header__profile">
+                    <img src={HeaderImage} alt="Header portrait" />
+                </div>
+            <h3>Nombre</h3>
+            <p>Parrafada sobre lo guay que soy como programadora, que soy tan buena que si me da la gana te programo la nevera para hacerte unas lentejas</p>
+            <div className="header__cta">
+                <a href="#contact">Contact</a>
+                <a href="#portfolio">Portfolio</a>
+            </div>
+            <div className="header__socials">
+                {
+                    data.map(item => <a key={item.id} href={item.link} target='_blank' rel='noopener noreferrer'>{item.icon}</a>)
+                }
+            </div>
+            </div>
+        </header>
     )
 }
 
