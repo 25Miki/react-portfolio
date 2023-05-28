bee59fd84e5d5437f97c6435469576f100fc6cc0
25Miki <mariamarchantellopis@gmail.com>
2023-05-29 00:01:11 +0200

    mañana más

--- src/sections/portfolio/Project.jsx
+++ src/sections/portfolio/Project.jsx
@@ -1,20 +1,32 @@
 import Card from '../../components/Card'
 
-
-const Project = ({project}) => {
-  return (
-    <Card className="portfolio__project">
+const Project = ({ project }) => {
+  if (project.category === "art") {
+    return (
+      <Card className="portfolio__project">
         <div className="portfolio__project-image">
-            <img src={project.image} alt="Portfolio Project Image" />
+          <img src={project.image} alt="Portfolio Project Image" />
         </div>
         <h4>{project.title}</h4>
         <p>{project.desc}</p>
-        <div className="portfolio__project-cta">
-            <a href={project.demo} className="btn sm" target="_blank" rel="noopener noreferrer">Demo</a>
-            <a href={project.github} className="btn sm primary" target="_blank" rel="noopner noreferrer">Gitbub</a>
-        </div>
-    </Card>
-  )
-}
+      </Card>
+    );
+  }
 
-export default Project
+  return (
+    <Card className="portfolio__project">
+      <div className="portfolio__project-image">
+        <img src={project.image} alt="Portfolio Project Image" />
+      </div>
+      <h4>{project.title}</h4>
+      <p>{project.desc}</p>
+      <div className="portfolio__project-cta">
+        <a href={project.github} className="btn sm primary" target="_blank">
+          Github
+        </a>
+      </div>
+    </Card>
+  );
+};
+
+export default Project;
