70aed3df37086e95db4da1f7a45ecba2c0d8ec91
25Miki <mariamarchantellopis@gmail.com>
2023-06-04 11:44:58 +0200

    ??

--- src/sections/portfolio/ProjectsCategories.jsx
+++ src/sections/portfolio/ProjectsCategories.jsx
@@ -1,23 +1,35 @@
-import { useState } from 'react'
-import CategoryButton from './CategoryButton'
+import { useState } from 'react';
+import CategoryButton from './CategoryButton';
 
-const ProjectsCategories = ({categories, onFilterProjects}) => {
-    const [activeCategory, setActiveCategory] = useState('all');
+const ProjectsCategories = ({ categories, onFilterProjects }) => {
+  const [activeCategory, setActiveCategory] = useState('all');
 
-    const changeCategoryHandler = (activeCat) => {
-        setActiveCategory(activeCat);
-        onFilterProjects(activeCat);
+  const toggleCategory = (category) => {
+    // Si la categoría activa es la misma que la seleccionada, establecer 'all' como categoría activa
+    // y llama a la función onFilterProjects con 'all' como argumento
+    if (activeCategory === category) {
+      setActiveCategory('all');
+      onFilterProjects('all');
+    } else {
+      // De lo contrario, establecer la categoría seleccionada como categoría activa
+      // y llama a la función onFilterProjects con la categoría seleccionada como argumento
+      setActiveCategory(category);
+      onFilterProjects(category);
     }
+  };
 
   return (
-    <div className='portfolio__categories'>
-        {
-            categories.map(category => (
-                <CategoryButton key={category} category={category} onChangeCategory={() => changeCategoryHandler(category)} className={`btn cat__btn ${activeCategory == category ? 'primary' : 'white'}`}/>
-            ))
-        }
+    <div className="portfolio__categories">
+      {categories.map((category) => (
+        <CategoryButton
+          key={category}
+          category={category}
+          onChangeCategory={() => toggleCategory(category)}
+          className={`btn cat__btn ${activeCategory === category ? 'primary' : 'white'}`}
+        />
+      ))}
     </div>
-  )
-}
+  );
+};
 
-export default ProjectsCategories
+export default ProjectsCategories;
